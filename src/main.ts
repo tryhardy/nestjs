2fcaa23b1ebb3737e1d76bcbe09e4300d600cd95
Наталия Гаращенко <n.garashchenko@uplab.ru>
2023-06-17 16:10:00 +0300

    Выполнено задание 1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor)
   await app.listen(process.env.PORT);
 }
 bootstrap();
